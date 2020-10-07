63c41bbd9617f1df8abde3b1f9aabb58c4b96cfc
Pavel Piasetski <pyasecky2010pavel@mail.ru>
2020-10-07 20:19:31 +0300

    Changed uncommented places

--- Telebot_location.py
+++ Telebot_location.py
@@ -206,11 +206,11 @@ def handler_message(message):
 # Enable saving next step handlers to file "./.handlers-saves/step.save".
 # Delay=2 means that after any change in next step handlers (e.g. calling register_next_step_handler())
 # saving will hapen after delay 2 seconds.
-bot.enable_save_next_step_handlers(delay=2)                                                                #Need to uncomment
+#bot.enable_save_next_step_handlers(delay=2)                                                                #Need to uncomment
 
 # Load next_step_handlers from save file (default "./.handlers-saves/step.save")
 # WARNING It will work only if enable_save_next_step_handlers was called!
-bot.load_next_step_handlers()                                                                     #Need to uncomment
+#bot.load_next_step_handlers()                                                                     #Need to uncomment
 
 if __name__ == '__main__':
     bot.polling(none_stop= True)
